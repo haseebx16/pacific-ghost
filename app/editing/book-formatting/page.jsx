d1a72edaf6313ffc8c3f3732f545ca7f854ef5fc
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-06 23:33:40 +0500

    Update page.jsx

--- app/editing/book-formatting/page.jsx
+++ app/editing/book-formatting/page.jsx
@@ -91,8 +91,9 @@ const page = () => {
         We are a known name in the literature industry and among our clients. With a hundred percent client satisfaction rates, we are proud to be the best formatting service provider. Pacific Ghostwriting has years of experience in the field and it is one of the reasons why the client chose us from the other options available.
         </p>
         <p className="text-gray-500 mt-2 text-sm">
-        We are a known name in the literature industry and among our clients. With a hundred percent client satisfaction rates, we are proud to be the best formatting service provider. Pacific Ghostwriting has years of experience in the field and it is one of the reasons why the client chose us from the other options available.
-        </p>
+        Now is your chance to get your manuscript formatted by Pacific Ghostwriting and ensure that the book will reach your readers with a flow that will make their reading experience smooth.
+
+</p>
 
         </div>
       <ContactBanner/>
