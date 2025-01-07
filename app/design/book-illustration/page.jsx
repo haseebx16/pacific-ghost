d83e058f99d7269800161be3313fb54f91299b6e
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-07 19:43:45 +0500

    Update page.jsx

--- app/design/book-illustration/page.jsx
+++ app/design/book-illustration/page.jsx
@@ -66,7 +66,7 @@ const page = () => {
         </p>
 
         <h1 className="text-3xl md:text-4xl mt-12">Affordable Book Illustration Services – Find All at One Place
-        </h1>
+</h1>
         <p className="text-gray-500 mt-2 text-sm">
             At Pacific Ghostwriting, we offer a diverse range of autobiography writing services personalized to your unique needs and preferences. Our team lets you choose the desired format and style you want for your autobiography.
         </p>
