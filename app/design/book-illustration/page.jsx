e80ba8cc184836c66526a0fac43b343a6a7ae4e2
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-07 19:25:01 +0500

    Update page.jsx

--- app/design/book-illustration/page.jsx
+++ app/design/book-illustration/page.jsx
@@ -40,7 +40,7 @@ const page = () => {
         <h1 className="text-3xl md:text-4xl mt-12">An Ocean of Talent – Children’s Book Illustrators for Hire        </h1>
         <p className="text-gray-500 mt-2 text-sm">
         Book Illustration Services are a major part of children’s books. At Pacific Ghostwriting we have a talented team of illustrators who hand trace alluring images creating visionary characters
-      </p>
+        </p>
         <p className="text-gray-500 mt-2 text-sm">
         At Pacific Ghostwriting, every story is discussed in a unique tone. Even if you are a business leader, a public figure, or someone with a remarkable journey to tell, we are dedicated to turning your memories into a literary masterpiece. Our personalized approach confirms that your voice shines through every page, creating a narrative that readers want
         </p>
