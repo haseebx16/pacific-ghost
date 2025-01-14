81bcdbab52ef8e1cace80348a97dea0edc0fc7f6
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-15 03:23:55 +0500

    Update page.jsx

--- app/services/non-fiction/page.jsx
+++ app/services/non-fiction/page.jsx
@@ -26,73 +26,73 @@ const page = () => {
         </div>
       </div>
       <div className="pt-12 w-[70%] pb-12 mx-auto md:mx-4">
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-justify text-sm">
         Are you passionate about sharing your story with the world but lack the time or writing expertise to bring it to life? Look no further! Our non-fiction ghostwriting services are here to help turn your vision into a captivating manuscript that captivates readers and leaves a lasting impact.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         If you are struggling with writing a non-fiction book on your own, no worries we have got your back. Our realm of literary expertise has ghostwriters who have mastery of covering all the widely popular genres and subgenres of non-fiction. Our non-fiction writers for hire are all geared up to provide you with exceptional writing services as a result of their years of expertise.
         </p>
        
         {/* Section 1 */}
-        <h1 className="text-3xl md:text-4xl mt-12">Why Choose Our Ghostwriting Services?</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Why Choose Our Ghostwriting Services?</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         At Coastal Ghostwriting, we understand the importance of preserving your unique voice and perspective. Our team of experienced ghostwriters specializes in crafting compelling non-fiction narratives that resonate with readers. Whether you’re a business leader, entrepreneur, expert in your field, or someone with a remarkable life story to tell, we’re here to help you share your wisdom, insights, and experiences with the world.
         </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">Our Transparent Approach to Creating Your Masterpiece</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Our Transparent Approach to Creating Your Masterpiece</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We believe in a collaborative approach to ghostwriting, where your vision remains at the forefront of the creative process. Our skilled Nonfiction Writers work closely with you to understand your objectives, tone, and style, ensuring that the final manuscript authentically reflects your voice. From the initial brainstorming session to the final draft, we’re committed to delivering a manuscript that exceeds your expectations.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We value every suggestion given by authors and imply that using our best potential. We aim to deliver nothing but quality.
         </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">Unmatched Services We Offer In Non-Fiction</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Unmatched Services We Offer In Non-Fiction</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         You can get a quote on any of the best nonfiction book writing services we have mentioned on our page. Our services are second to none and give you complete author’s rights without claiming for ourselves.
         </p>
         
 
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Book Ghostwriting</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         From memoirs and autobiographies to self-help books and business guides, we specialize in bringing your non-fiction book ideas to life. We guarantee success in all the areas of literature and the world outside its axis. Our nonfiction ghostwriting services are all about offering uniqueness and authentic ideas.
         </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Manuscript Editing</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Already have a draft? Our team of editors can review and polish your manuscript to enhance clarity, coherence, and readability. We have a bunch of experts with an eagle’s eye view. Our experts won’t judge your writing style but try their level best to give you a copy that resonates with your writing style.
         </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Proposal and Query Letter Writing</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Need help pitching your book to literary agents and publishers? We’ll craft compelling proposals and query letters that showcase the unique value of your project.
         </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Consultation and Coaching</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Throughout the writing and publishing process, our experts are here to provide guidance, support, and encouragement every step of the way. We keep the clients posted in between the process and are all ears to suggestions and edits.
         </p>
 
-        <h1 className="text-3xl md:text-4xl mt-12">Meet Our Brilliantly Talented Team Of Writers</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Meet Our Brilliantly Talented Team Of Writers</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Behind every successful project is a dedicated team of professional and best Non-Fiction writers. Meet our talented team of professional non-fiction ghostwriters, editors, and publishing experts who are passionate about helping you bring your story to life.
         </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">Client Testimonials</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Client Testimonials</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Don’t just take our word for it! Hear what our satisfied clients have to say about their experience working with us. We have hundreds of happy and satisfied clients from across the globe. We entertain clients from all cultures and ethnicities.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Our Outstanding Portfolio</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Our Outstanding Portfolio</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Explore samples of our past projects to get a glimpse of our writing style and quality craftsmanship. Our portfolio is the result of years of hardships, dedication, and the energy our writers have put into the projects. Now that’s for you to go through and choose us for your next project.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Discover Our Process, Pricing, and Packages</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Discover Our Process, Pricing, and Packages</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Learn more about our ghostwriting process, pricing structure, and how to get started on your project today. Hit us by email or ring our desks, we will be catering to your needs 24/7.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Got A Question In Mind?</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Got A Question In Mind?</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Have questions? We’ve got answers! Check out our frequently asked questions section for more information on confidentiality, copyright, revisions, and more. If you have any other query apart from the discussed one, hit us on the phone or send an email. We will reach out and assign you one of the literary wizards to look after your concern.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Contact Us And Discuss Your Book With Our Writers</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Contact Us And Discuss Your Book With Our Writers</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Ready to take the next step? Contact us today for a free consultation and discover how our non-fiction ghostwriting services can help you turn your story into a compelling book that inspires, educates, and entertains readers around the world. Let’s bring your vision to life together!
         </p>
         
