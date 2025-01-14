2840b9aa7d8e9c9e2873e0aedd8b620669b1f90a
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-15 03:46:39 +0500

    Update page.jsx

--- app/editing/book-proofreading/page.jsx
+++ app/editing/book-proofreading/page.jsx
@@ -27,32 +27,32 @@ const page = () => {
         </div>
       </div>
       <div className="pt-12 w-[70%] pb-12 mx-auto md:mx-4">
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-justify text-sm">
         Proofreading is not a simple step and your book depends on it. Proofreading services are the best way to make sure that your script doesn’t have any silly mistakes that might trigger your authority or credibility.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         Our book proofreading services make sure that when you have the final draft of your book in your hands – it is free of any mistakes. It is indefensible for the author to serve as a quality control measure that enhances the overall standards of your work.
         </p>
 
 
         {/* Section 1 */}
-        <h1 className="text-3xl md:text-4xl mt-12">Professional Proofreaders Guiding Light Towards Finest Manuscript
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Professional Proofreaders Guiding Light Towards Finest Manuscript
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         As one of the best book proofreading services in the town, we adhere to an accurate process for proofreading to ensure the quality and accuracy of your document. Our proofreading stages are very essential for maintaining our professional standards and upholding our commitment to excellence.
         </p>
        
         
         <h1 className="text-3xl md:text-4xl mt-12">Initial Review
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We commence the proofreading process with an initial review of the manuscript. During this stage, our proofreads focus on the content, context, and purpose of your book. We must grasp the key message and intended audience before we proceed further.
       </p>
         
         
-        <h1 className="text-3xl md:text-4xl mt-12">Structural Evaluation
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Structural Evaluation
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Then comes the structural evaluation stage after the initial review of your manuscript. In this stage, our team assesses the organization, coherence, and flow of the content. We aim to make sure that the information and data present in your manuscript are logical and effective to the readers, enhancing readability and compression.
 
 </p>
@@ -60,7 +60,7 @@ const page = () => {
 
         <h1 className="text-3xl md:text-4xl mt-12">Content Analysis
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         This is the third stage of the proofreading procedure at Coastal Ghostwriting. At this stage, our proofreader conducts a structural evaluation of your manuscript. We scrutinize the accuracy, relevance, and completeness of the information presented. Any inconsistencies, factual errors, or discrepancies are identified and addressed in this stage.
 
 </p>
@@ -68,50 +68,50 @@ const page = () => {
 
 
       
-        <h1 className="text-3xl md:text-4xl mt-12">Language And Style Check</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Language And Style Check</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our next step involves a throughout language and style check. We pay close attention to grammar, punctuation, spelling, and syntax. In addition to this, we also make sure that consistency in formatting tone, and style throughout the manuscript, aligns with our company’s guidelines and standards.
 
 </p>
         
         <h1 className="text-3xl md:text-4xl mt-12">Fact-Checking And Verification
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our best book proofreaders make sure that fact-checking and verification are up to the mark. We verify factual information, references, and citations to maintain the credibility and reliability of your book. Any type of inaccuracy or ambiguity is rectified frequently to uphold the integrity of your content.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Final Review And Quality Assurance
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Final Review And Quality Assurance
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Before we finalize the manuscript, we conduct a comprehensive review to address any remaining issues or concerns. Our team of professional proofreaders examines every aspect of the document to ensure accuracy, clarity, and coherence. Our focus is on delivering a polished and error-free final product that meets the expectations of the clients and reflects our commitment to excellence.
         </p>
         <h1 className="text-3xl md:text-4xl mt-12">Fresh Perspective
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         After spending a specific time writing your book it is easy to overlook errors or become blind to them. Our professional proofreaders bring a fresh set of eyes to your manuscript and catch the mistakes that you might have neglected.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Attention To Detail</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Attention To Detail</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our proofreaders are trained to pay attention to the smallest details which include grammar, punctuation, spelling, and syntax errors. We have a keen eye for inconsistencies and can make sure that your book maintains a high level of professionalism and reliability.
         </p>
         <h1 className="text-3xl md:text-4xl mt-12">Enhanced Credibility
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         A book that is free of errors and inconsistencies is more likely to be perceived as credible and authoritative by readers. Our professional proofreading adds a layer of polish and professionalism to your book and enhances its reputation and appeal.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Consistency        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Consistency        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We have a team of expert proofreaders who ensure consistency throughout your book including style, formatting, and language use. We adhere to the specific guidelines or preferences you have and make sure that your book maintains a cohesive and polished appearance to the readers.
         </p>
         <h1 className="text-3xl md:text-4xl mt-12">Language Expertise        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We have experts in language and grammar rules. Our team can identify grammatical errors, awkward phrasing, and ambiguous sentences helping to clarify your writing and improve overall comprehension.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Connect With Our Expert Proofreaders For Hire
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Connect With Our Expert Proofreaders For Hire
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We invite you to connect with our finest and most talented proofreaders and enhance the overall charm of your book. Our team of proofreaders is here to help you with everything you need. We ensure that everything about your book complies with the publishing standards and also facilitate proofreading effectively.
         </p>
-        <p className="text-gray-500 italic mt-2 text-sm">
+        <p className="text-gray-500 text-justify italic mt-2 text-sm">
         We are here to support you and sort out any query related to proofreading – all you have to do is contact us to Proofreaders for hire and leave everything to our experts.
       </p>
 
