56cd3fd8ffb7475e8c17265fd97be2551797279c
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-15 03:37:02 +0500

    Update page.jsx

--- app/services/business-book/page.jsx
+++ app/services/business-book/page.jsx
@@ -26,32 +26,32 @@ const page = () => {
         </div>
       </div>
       <div className="pt-12 w-[70%] pb-12 mx-auto md:mx-4">
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-justify text-sm">
         Your idea can help millions to succeed in their business if you count on Coastal Ghostwriting’s top business book writers. We, here turn your idea into a roadmap for success that helps entrepreneurs thrive worldwide.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         It’s no secret that reading is essential for personal and professional development, and the same goes for business books. These books are powerful tools created to inspire, inform, and give people knowledge about running a successful business.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         These books are carefully created by industry experts, CEOs, entrepreneurs, and thought leaders who want to share their wisdom, experiences, and insights with a broader audience. However, not everyone has the time or expertise to write a perfect business book.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         Here at Coastal Ghostwriting, we’re experts in delivering top-notch business book ghostwriting services. With our talented team of writers, editors, and publishing experts, you can share your experiences in a way that helps newcomers to learn and succeed.
         </p>
 
 
 
         {/* Section 1 */}
-        <h1 className="text-3xl md:text-4xl mt-12">Our Business Book Writers Turn Your Experiences And Expertise Into A Best Business Guide</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Our Business Book Writers Turn Your Experiences And Expertise Into A Best Business Guide</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Are you a business owner looking to share your expertise and help newcomers succeed? Here’s your opportunity! Collaborate with us at Coastal Ghostwriting to bring your ideas to life in a book that guides and supports aspiring entrepreneurs.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our team of best business book writers is always passionate about working with you and bringing your experiences to life on the pages of your book. We appreciate your journey as an industry expert is filled with valuable insights, lessons learned, and achievements worth sharing. That’s why we take the time to deeply understand your background, your vision for the book, and the messages you want to convey.
         </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">The Diverse Range Of Business Book Services</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">The Diverse Range Of Business Book Services</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Coastal Ghostwriting offers a variety of ghostwriting services for various types of business books, each tailored to meet your specific needs perfectly. While we can’t list them all, here are some examples,
         </p>
         <p className="text-gray-500 space-y-2 mt-4 flex flex-col text-sm">
@@ -62,41 +62,41 @@ const page = () => {
           <span className='font-medium text-gray-500'>&#x2022;&nbsp;Innovation and creativity - Inspiring new ideas and approaches in business.</span> 
         </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">Here is How Our Business Book Writing Services Work</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Here is How Our Business Book Writing Services Work</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We take a thorough approach to writing your business book, making sure we cover everything needed to make it truly impactful and transformative.
         </p>
 
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Strategy & Outlining Session</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We start by planning and outlining your book. This involves understanding your objectives, target audience, key messages, and desired outcomes. We create a detailed plan that serves as a roadmap for the writing process.
         </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Interviews</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our team then conducts in-depth interviews with you to know the experiences and expertise you want to add to your book. These interviews help us capture your voice, expertise, and unique perspective in the manuscript.
         </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Ghostwriting</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         With the outline and interviews, our talented business book ghostwriters start writing your book. They skillfully transform your ideas and experiences into compelling writing while maintaining your voice every step of the way.
         </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Editing & Proofreading</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         After completing the first draft, our editors carefully review the book. They concentrate on improving the content, clarity, coherence, and flow, which as a result guarantees grammatical accuracy. Proofreading is also conducted to make your book written perfectly.
         </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Manuscript Delivery</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Once we complete the editing process, we finalize the manuscript. We then deliver the finished business book to you in your preferred format, ready for publication or further distribution.
         </p>
 
-        <h1 className="text-3xl md:text-4xl mt-12">Our Business Book Writers Help You To Become A Published Author</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Our Business Book Writers Help You To Become A Published Author</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our experienced team of business book writers is highly passionate about making your dream of becoming a published author a reality. We support our clients taking all their burdens on our shoulders.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         From conceptualization to completion, we work tirelessly to create a compelling book that highlights your expertise and connects with your readers. Whether you choose self-publishing or pursue traditional publishing routes, we offer the assistance and tools necessary to navigate the publishing journey successfully.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Let’s Quote Us Anytime!</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Let’s Quote Us Anytime!</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Don’t wait any longer and quote a talented team of business book ghostwriting services. Coastal Ghostwriting is the best business book ghostwriting company available any time to listen to your ideas and turn them into an attention-grabbing narrative.
         </p>
 
