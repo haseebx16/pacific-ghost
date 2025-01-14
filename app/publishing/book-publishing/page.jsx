b728cea334c782c2bbb5c982226aa3afcd089b11
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-15 04:08:08 +0500

    Update page.jsx

--- app/publishing/book-publishing/page.jsx
+++ app/publishing/book-publishing/page.jsx
@@ -27,14 +27,14 @@ const page = () => {
         </div>
       </div>
       <div className="pt-12 w-[70%] pb-12 mx-auto md:mx-4">
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-justify text-sm">
         Need help with book publishing? Coastal Ghostwriting is here to guide you through this bumpy road of publishing. We navigate authors and guide them through a path filled with success. Authors need to trust the process, our book publishers, and our methods.
 
 </p>
-        <p className="text-gray-500 italic mt-6 text-sm">
+        <p className="text-gray-500 text-justify italic mt-6 text-sm">
         “Publishing a book is like stuffing a note into a bottle and hurling it into the sea. Some bottles drown, and some come safe to land, where the notes are read and then possibly cherished, or else misinterpreted, or else understood all too well by those who hate the message. You never know who your readers might be.”
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         Just like this author on Goodreads understands the challenges and risks of book publishing and the risks involved in the path. We understand your concerns as well, all we request is to trust in the process and our book publishers.
 
 </p>
@@ -42,90 +42,90 @@ const page = () => {
 
 
         {/* Section 1 */}
-        <h1 className="text-3xl md:text-4xl mt-12">About Us- Our Vision To Make Your Book Successful
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">About Us- Our Vision To Make Your Book Successful
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         At Coastal Ghostwriting, we believe that every story deserves to be told. Founded by a team of industry experts with a shared love for books, our mission is to empower authors to share their stories with the world. With years of experience and a dedication to quality, we pride ourselves on our reputation for professionalism, integrity, and creativity.
 
 </p>
       
-        <h1 className="text-3xl md:text-4xl mt-12">Our Services- Known Among The Best Book Publishing Companies
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Our Services- Known Among The Best Book Publishing Companies
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Whether you’re a first-time author or an established writer, we offer a comprehensive range of publishing services to meet your needs. From manuscript evaluation and editing to design, printing, and distribution, we’ll work closely with you to bring your vision to life. Our marketing and promotion services will help you reach your target audience and maximize the impact of your book.
 
 </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         The Coastal Ghostwriting ook publishing company is designed to be transparent, efficient, and collaborative. From the moment our client submits their manuscript, we guide them through each stage of the publishing journey, ensuring that their vision is appreciated every step of the way. With regular communication and virtual meetings, we’ll help authors navigate the complexities of the publishing industry with ease.
 
 </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">Our Talented Team Of Writers, Publishers And Editors
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Our Talented Team Of Writers, Publishers And Editors
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         At [Publishing Company Name], our authors are at the heart of everything we do. We’re proud to have worked with a diverse range of talented writers, each with a unique voice and story to tell. Whether you’re a bestselling author or a debut novelist, we’re committed to providing you with the support and resources you need to succeed.
 
 </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Choose from our range of publishing packages to find the one that best suits your needs and budget. Whether you’re looking for basic publishing services or a comprehensive package with value-added benefits, we have a variety of options to fit every author’s needs. Our transparent pricing and flexible packages make it easy to get started on your publishing journey. We are known best publishers for new authors in the town for keeping the pricing low.
 
 </p>
 
-        <h1 className="text-3xl md:text-4xl mt-12">Have A Look At Our Best Book Publishing Services Submissions Guidelines
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Have A Look At Our Best Book Publishing Services Submissions Guidelines
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Ready to submit your manuscript? Our submission guidelines will walk you through the process step by step. From formatting requirements to submission deadlines, we’ll provide you with all the material you need to prepare your manuscript for reflection. We’re always on the lookout for new and exciting voices, so don’t hesitate to reach out to us with your work.
 
 </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Have Questions About The Publishing Process?
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Have Questions About The Publishing Process?
 
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Have Questions About The Publishing Process?
         </p>
 
 
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Proofreading
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         This step involves proofreading the manuscript and making sure no errors or flaws are left behind. After this, we move on to the editing process.
 
 </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Editing
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We focus on structural editing, content editing, and technical editing. The editing focuses on the way your manuscript is prepared and whether it follows the standard.
 
 </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Revisions
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Despite all the editing and proofreading, revisions never stop. Our experts reread all the texts to make sure no room is left for any error. Humans can make errors though, but we still keep an eagle’s eye.
 
 </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Design And Typesetting
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Design and typesetting include the book cover, layout, book illustration, and typography. We will send you the designs as well to make sure it approved by you.
 
 </p>
         <h1 className="text-2xl font-bold md:text-3xl mt-8">Marketing And Promotions
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         No matter how brilliantly the book has been written, it still needs to be promoted on all the leading platforms including social media. We will make sure your book goes through the eyes of all the bookworms.
 
 </p>
 
-        <h1 className="text-3xl md:text-4xl mt-12">Contact Us For Your Publishing Concern
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Contact Us For Your Publishing Concern
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Ready to take the next step on your publishing journey? Contact us today to learn more about our services or to schedule a consultation. We’re here to answer your questions, address your concerns, and help you get started on the path to publishing success.
 
 </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">Making Your Publishing Dream A Living Reality
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Making Your Publishing Dream A Living Reality
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Try considering Coastal Ghostwriting for your publishing needs. We’re excited to partner with you on your journey to becoming a published author. With our expertise, dedication, and passion for storytelling, we’re confident that together, we can bring your vision to life and make your publishing dreams a reality.
 
 </p>
