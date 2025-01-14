73704dfa9bc631ae67cedb7c077a800d48fc8172
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-15 03:33:00 +0500

    Update page.jsx

--- app/services/song-writing/page.jsx
+++ app/services/song-writing/page.jsx
@@ -26,68 +26,68 @@ const page = () => {
         </div>
       </div>
       <div className="pt-12 w-[70%] pb-12 mx-auto md:mx-4">
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-justify text-sm">
         Have you hired the songwriter yet? Well, why are you waiting? when you have us. Forget about looking around the market to meet numerous songwriting services, Coastal Ghostwriting is here. Songs increase our emotions and generate them. Have you ever thought Elton John or Bob Dylan created such a mesmerizing song that captivates their listeners? Well, they know how to write a song. A perfect set of lyrics, and a beautifully composed song, there you have a top record song.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         Doesn’t it sound good? Well, yes it does and we can make it happen for you. We have ghost songwriters immersed in the art of writing thematic songs. Hire us so we can create rich, original music lyrics where skies and stars create a magical world. Songwriting is all about putting the right words together. So, do not wait more, and enjoy Coastal Ghostwriting songwriting services
         </p>
 
         {/* Section 1 */}
-        <h1 className="text-3xl md:text-4xl mt-12">Enjoy Professional Songwriters Service at Coastal Ghostwriting</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Enjoy Professional Songwriters Service at Coastal Ghostwriting</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         You may have thought about an incident which you would like to sing about. How would you do it? People like to listen more than they like to read. So, hire a songwriter from Coastal Ghostwriting and we will reflect you through a song.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Songwriting is a tough job. It takes the songwriter to have such skill where he has to reflect the voice of others. However, we know how to do it. Working for years now we cover all genres in songs. The team of songwriters are music lovers so they adjust well to their task.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We are here to bring your music vision to life; our team goes up and beyond in bringing melodies together for you. We will cater to you with affectionate, and powerful lyrics that people will not forget.
         </p>
         
         <h1 className="text-3xl md:text-4xl mt-12">Best Songwriting Services for You to Relieve Emotions – Why Choose Us?</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Multiple reasons tell why you need to choose us. You will be impressed to know how we work and what makes us different from others. So, let us look in some reason we want to tell you,
         </p>
         
         
-        <h1 className="text-3xl md:text-4xl mt-12">Professional Songwriting Services</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Professional Songwriting Services</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our priority is professionalism, it is what we master. We have a team of people who are experienced in songwriting for years. So, we know how and what it takes to create a song that stands out and connects with the listeners.
         </p>
 
         <h1 className="text-3xl md:text-4xl mt-12">Tailored Approach</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We are not blithe about your work; we carefully work with each of our clients. When you get on board our team ensures everything is clear between us, and no anomaly is found. It takes us to go the extra mile in creating custom services that fulfill each demand and create the song of your dreams.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Collaborative Process</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Collaborative Process</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our company believes in collaboration and welcomes your input every step of the way. From brainstorming sessions to revisions and feedback. We work closely with you to ensure that the final product reflects your vision and exceeds your expectations.
         </p>
         <h1 className="text-3xl md:text-4xl mt-12">Diverse Genres</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Whether you like pop, rock, hip-hop, country, R&B, or any other genre, we can cater to you. Our amazingly versatile songwriters have experience across a wide range of musical styles, allowing us to create songs that resonate with diverse audiences.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Affordable Pricing</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Affordable Pricing</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         our pricing is decided after looking at the market, and its conditions. We offer competitive pricing tailored to the client’s demands with great deals for you.
         </p>
 
 
         <h1 className="text-3xl md:text-4xl mt-12">Custom Songwriting</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Allow us to create original songs tailored to your style, and voice, in your song. Our custom songwriting is carried out by a separate panel that works on transforming words into essential lyrics. As a leading songwriting service, you can consider us songwriters for hire
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Lyric Writing</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Lyric Writing</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Need help expressing your thoughts and emotions in lyrical form? We have the best lyrics in town. Our song ghostwriters are well-versed in writing compelling lyrics.
         </p>
         <h1 className="text-3xl md:text-4xl mt-12">Melody Composition</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our skilled composers can craft memorable melodies that leave a lasting impression for years and for generations to pass on. Remember Otis Black?
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Song Arrangement</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Song Arrangement</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         From chord progressions to song structure, we ensure that every aspect of your song flows seamlessly without hitting the rocks. Our song arrangements are carried out by our songwriters as well as composers who understand the intricacies of song composition.
         </p>
         
