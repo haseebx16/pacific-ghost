4c432ad8847ab53fdd1e0073cf294c88bf810a9d
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-15 03:50:19 +0500

    Update page.jsx

--- app/editing/book-formatting/page.jsx
+++ app/editing/book-formatting/page.jsx
@@ -26,71 +26,71 @@ const page = () => {
         </div>
       </div>
       <div className="pt-12 w-[70%] pb-12 mx-auto md:mx-4">
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-justify text-sm">
         Formatting in a book is essential for reader engagement and comprehension. It makes sure that there is clarity in your content. Formatting makes it easier for your readers to understand and acquire a flow that keeps the readers engaged.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         And for readers, a book with proper formatting is not good – they might lose their interest just after a few pages. It ensures the necessity of formatting in your book.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         Although it might be hard to work on formatting on book by yourself you should take professional help to make things easier for you. Coastal Ghostwriting is here to keep up with your demands and ensure that all your formatting needs are fulfilled.
         </p>
 
 
 
         {/* Section 1 */}
-        <h1 className="text-3xl md:text-4xl mt-12">Why To Choose Our Book Formatting Services
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Why To Choose Our Book Formatting Services
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Coastal Ghostwriting is the perfect place for you to avail of the formatting services for your book. We have a lot of things to convince you why we are the best option for you.
         </p>
         
         <h1 className="text-3xl md:text-4xl mt-12">We Have Expertise
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our team is composed of the finest professionals with extensive experience in formatting various types of books – whether it is your life journey notes or a craft of your imagination. We stay updated with industry trends and best practices to make sure that your manuscript adheres to the latest formatting standards.
         </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">We Go For A Customized Approach        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">We Go For A Customized Approach        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our team understands that every book’s formatting differs from the others and has unique requirements and objectives. Whether you want to write a book for business or academic purposes or want to explore your creative spark – we work closely with you to understand your vision and tailor the formatting solutions accordingly.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         From font styles and colors to margins and spacing, we customize every element to align with your preferences and branding guidelines.
       </p>
 
         <h1 className="text-3xl md:text-4xl mt-12">We Are Always On Time        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Deadlines matter and we as the proofreading service provider know the importance of delivering your formatted book promptly. Our team organizes the workflow and efficient processes that enable us to meet even the tightest deadlines without compromising quality. You can rely on us to deliver your formatted manuscript on time.
         </p>    
         
-        <h1 className="text-3xl md:text-4xl mt-12">We Notice Every Detail        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">We Notice Every Detail        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our team of experts knows how easy it is to neglect a simple mistake. That is why, we leave no stone unturned when it comes to perfecting your manuscript. Whether it is to ensure consistency in formatting styles or check for typographical errors or alignment issues – we accurately review every aspect to maintain a high standard of quality and professionalism.
         </p>
         <h1 className="text-3xl md:text-4xl mt-12">We Aim For Customer Satisfaction
         </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Your satisfaction is at the heart of everything we do. We value open communication and collaboration with our clients and actively seek feedback throughout the formatting process to make sure that we meet and exceed your expectations.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">We Offer Competitive Pricing        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">We Offer Competitive Pricing        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         It might be hard to know the exact book formatting cost as different factors are there to consider to know the exact amount. But we ensure that with quality formatting, our pricing packages are designed to offer exceptional value for your investment and provide transparent pricing structures with no hidden charges.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We always believe in offering competitive rates without compromising on the quality of our services and make professional formatting accessible to everyone.
       </p>
         <h1 className="text-3xl md:text-4xl mt-12">Enjoy Coastal Ghostwriting Professional Book Formatting Services For Flawless Stories</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         When you connect with Coastal Ghostwriting, we make sure that you get the finest formatting services. The professionals at our company know the value of formatting when it comes to books and that is the reason, they focus on offering their best to you.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Apart from this, we mostly use professional writing software and tools with formatting features to organize the whole process and achieve consistent results. these tools help maintain uniformity throughout the manuscript and make sure that formatting errors are minimized or eliminated.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         We are a known name in the literature industry and among our clients. With a hundred percent client satisfaction rates, we are proud to be the best formatting service provider. Coastal Ghostwriting has years of experience in the field and it is one of the reasons why the client chose us from the other options available.
         </p>
-        <p className="text-gray-500 italic mt-2 text-sm">
+        <p className="text-gray-500 text-justify italic mt-2 text-sm">
         Now is your chance to get your manuscript formatted by Coastal Ghostwriting and ensure that the book will reach your readers with a flow that will make their reading experience smooth.
         </p>
 
