bb0ab18e03c35b8b91758d98ec4083cc0f196ce5
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-01-15 03:44:01 +0500

    Update page.jsx

--- app/editing/book-editing/page.jsx
+++ app/editing/book-editing/page.jsx
@@ -26,67 +26,67 @@ const page = () => {
         </div>
       </div>
       <div className="pt-12 w-[70%] pb-12 mx-auto md:mx-4">
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-justify text-sm">
         Editing is one of the most essential stages in the book creation. It ensures that there is clarity, coherence, and reader engagement. Coastal Ghostwriting is here to help you with its skilled editors to refine your manuscript and style of writing which makes it more compelling and consistent throughout.
         </p>
-        <p className="text-gray-500 mt-6 text-sm">
+        <p className="text-gray-500 text-justify mt-6 text-sm">
         Our editing services provide you with the best results and the outcome is just how you wish. We ensure that our editing process makes sure that your book is free of any lag and lacks nothing. Throughout the process, we keep our client close to us, so there is no communication gap and everything is according to the plan.        </p>
         
 
 
 
         {/* Section 1 */}
-        <h1 className="text-3xl md:text-4xl mt-12">Services We Offer As A Book Editing Company</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Services We Offer As A Book Editing Company</h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         As a professional editing service provider, we offer our clients the following services.
         </p>
         
         <h1 className="text-3xl md:text-4xl mt-12">Substantive Editing</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         It is also known as developmental editing and dives into the core aspects of your written work which mainly include its structure, content, and organization. It surpasses mere grammar and punctuation corrections to evaluate how effectively your manuscript communicates its intended message to your audience.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our professional book editors go through tasks such as structural assessment, content evaluation, and audience engagement analysis. This editing service by our book editing agency provides recommendations that are aimed at enhancing the clarity and engagement of your content with readers.
         </p>
         
-        <h1 className="text-3xl md:text-4xl mt-12">Structural Editing        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Structural Editing        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our structural Editing focuses on the macro-level elements of a manuscript that emphasize overall structure, the organization of content, and consistency. We scrutinize the arrangement of chapters, sections, and paragraphs to make sure that there is a logical progression and smooth transitions between the ideas.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         The editors also pay close attention to tone, style, and formatting consistency to maintain uniformity throughout the manuscript.
         </p>
 
         <h1 className="text-3xl md:text-4xl mt-12">Content Editing        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our professional book editing services also include content editing. It involves an accurate evaluation of substances, accuracy, and relevance in your book. Our editors conduct fact-checking, assess the depth of coverage, and edit content to suit your target audience’s needs and interests.
         </p>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Sensitivity and inclusivity considerations also make sure that the content aligns with diverse perspectives and develops a respectful environment for all readers.
         </p>
 
-        <h1 className="text-3xl md:text-4xl mt-12">Editorial Assessment        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Editorial Assessment        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         This service provides the author with a comprehensive overview of the strengths, weaknesses, and areas of improvement in their manuscript. It is the roadmap provided by our editors to you that guides you in refining your work, addressing structural, content-related, and stylistic concerns to enhance the overall effectiveness of your writing.
         </p>
         
         <h1 className="text-3xl md:text-4xl mt-12">Stylistic Editing</h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         In this type of editing, our writer focuses on refining language, tone, style, and expression to make sure that it enhances the readability and movement of your content. Our expert editors at Coastal Ghostwriting aim to convey the ideas more concisely and efficiently to make sure consistency in voice and style while enhancing flow and cohesion.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Evaluation Editing        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Evaluation Editing        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         It involves a very critical analysis of the quality, effectiveness, and impact of your book on the readers. In this editing type, our editors assess the coherence of arguments, anticipate readers’ responses, and then provide you with constructive feedback and recommendations for improvement grounded in objectivity and evidence.
         </p>
         <h1 className="text-3xl md:text-4xl mt-12">Mechanical Editing        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Mechanical editing also goes by the name copy editing. Here our editors focus on correcting mechanical errors or inconsistencies in grammar, punctuation, spelling, and formatting. The team at Coastal Ghostwriting makes sure to adhere to the language conventions and publishing standards to maintain clarity and precision throughout the manuscript.
         </p>
-        <p className="text-gray-500 italic mt-2 text-sm">
+        <p className="text-gray-500 text-justify italic mt-2 text-sm">
         Our editorial process encompasses a series of interrelated stages and each serves a distinct purpose to refine and enhance your written work. Whether it is substantive editing or mechanical, every type of editing contributes to the creation of a polished, coherent, and impactful manuscript that communicates with the audience more effectively.
         </p>
-        <h1 className="text-3xl md:text-4xl mt-12">Book Editors For Hire At Coastal Ghostwriting        </h1>
-        <p className="text-gray-500 mt-2 text-sm">
+        <h1 className="text-3xl md:text-4xl text-sky-500 mt-12">Book Editors For Hire At Coastal Ghostwriting        </h1>
+        <p className="text-gray-500 text-justify mt-2 text-sm">
         Our affordable book editors provide proficiency and apply their skills using the best tools to alter your manuscript. Coastal Ghostwriting is one of the book editing services that hold the reputation for being the finest in the industry. From expert editors to having a range of tools to elevate your book, it has everything to provide you with the best. We take pride as a book editing agency and maintain client satisfaction by offering affordable book editing services.
 
 </p>
